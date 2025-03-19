cb0077b1bc658f9fee7b333a3532db0203acef5d
jombi <jombi@duck.com>
2025-03-19 13:18:10 +0900

    feat :: publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,49 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     kotlin("jvm") version libs.versions.kotlin
-    id("java-library")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
 }
 
-/*publishing {
-    publications {
-        maven(MavenPublication) {
-            groupId = "com.github.kibertoad"
-            artifactId = "ktor-scheduler"
-            version = "1.0.2"
+group = "dev.jombi"
+version = "0.0.1"
 
-            from(components["java"])
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    coordinates(group.toString(), "ktor3-scheduler", version.toString())
+    signAllPublications()
+
+    pom {
+        name = "ktor3-scheduler"
+        description = " Cluster-friendly task-scheduler for ktor version 3"
+        url = "https://github.com/jombidev/ktor3-scheduler"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/license/mit"
+            }
+        }
+        developers {
+            developer {
+                id = "jombidev"
+                name = "Jombi"
+                email = "jombi@duck.com"
+            }
+        }
+        scm {
+            connection = "scm:git:git://github.com/jombidev/ktor3-scheduler.git"
+            developerConnection = "scm:git:ssh://github.com:jombidev/ktor3-scheduler.git"
+            url = "https://github.com/jombidev/ktor3-scheduler/tree/master"
         }
     }
-}*/
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -57,6 +84,6 @@ kotlin {
     compilerOptions {
         jvmToolchain(11)
         languageVersion.set(KotlinVersion.KOTLIN_2_0)
-        apiVersion.set(KotlinVersion.KOTLIN_1_9)
+        apiVersion.set(KotlinVersion.KOTLIN_2_0)
     }
 }
